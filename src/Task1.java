778db7f7172fb77fb89b4cf74d60245465920e9a
Valeriya Emelina <ilina.valeriya258@gmail.com>
2023-01-16 21:48:25 +0300

    Add code

---
+++ src/Task1.java
@@ -0,0 +1,12 @@
+public class Task1 {
+
+    public static void main(String[] args) {
+        int priceOfOneTicket = 3000;
+        int bonus = 20;
+        int miles = priceOfOneTicket / bonus;
+        System.out.println(miles);
+
+
+    }
+
+}
